d0ba1fac16d2e790457c51f7f77c2934a29d8077
KAMAL-02 <Kamalnayan403@gmail.com>
2025-02-08 22:31:08 +0530

    Authentication implemented

--- Backend/controller/authController.js
+++ Backend/controller/authController.js
@@ -2,12 +2,50 @@ const jwt = require('jsonwebtoken');
 const User = require('../model/user');
 const bcrypt = require('bcrypt');
 
-const signin = async(req, res) => {
-    res.send("Hello from signin");
+const signin = async(req) => {
+    const { name, email, password } = req.body;
+    if(!name || !email || !password){
+        return ({error : "Missing required fields"});
+    }
+
+    try{
+        const user = await User.findOne({email});
+        if(user) return ({error : "User already exists"});
+
+        const hashedPassword = bcrypt.hashSync(password, 10);
+        const newUser = await User.create({
+            name,
+            email,
+            password: hashedPassword
+        })
+
+        if(!newUser) return ({error : "User not created"});
+
+        const token = jwt.sign({id: newUser._id}, process.env.JWT_SECRET);
+        return ({ message: "User created successfully", newUser, token });
+    }catch(err){
+        console.log(err);
+        return ({error : err.message});
+    }
 }
 
-const login = async(req, res) => {
-    res.send("Hello from login");
+const login = async(req) => {
+    const { email, password } = req.body;
+    if(!email || !password) return ({error : "Missing required fields"});
+
+    try {
+        const user = await User.findOne({email});
+        if(!user) return ({error : "User not found"});
+
+        const isMatch = bcrypt.compareSync(password, user.password);
+        if(!isMatch) return ({error : "Invalid credentials"});
+
+        const token = jwt.sign({id: user._id}, process.env.JWT_SECRET);
+        return ({ message: "Login successful", user, token });
+    } catch (error) {
+        console.log(error);
+        return ({error : error.message});
+    }
 }
 
 module.exports = { signin, login };
--- Backend/route/authRoute.js
+++ Backend/route/authRoute.js
@@ -4,11 +4,29 @@ const { signin, login } = require('../controller/authController');
 
 
 router.post('/signin', (req, res) =>{
-    return signin(req, res);
+    signin(req).
+    then((response) => {
+        if(response.error){
+            res.status(400).json({...response});
+        }else{
+            res.status(200).json({...response});
+        }
+    }).catch((err) => {
+        res.status(400).json({...err});
+    })
 })
 
 router.post('/login', (req, res) => {
-    return login(req, res);
+    login(req)
+    .then((response) => {
+        if(response.error){
+            res.status(400).json({...response});
+        }else{
+            res.status(200).json({...response});
+        }
+    }).catch((err) => {
+        res.status(400).json({...err});
+    });
 })
 
 module.exports = router;
